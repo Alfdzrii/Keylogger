bde6542872e6270330978c7429557e12dcae7e79
Alfdzrii <Alfadzri.Abhipraya@gmail.com>
2025-12-29 15:31:07 +0700

    adding controller

---
+++ controll.py
@@ -0,0 +1,13 @@
+from pynput.mouse import Controller 
+from pynput.keyboard import Controller
+
+# controlling keyboard
+def controlKeyboard():
+    keyboard = Controller()
+    keyboard.type("halo ganteng")
+
+
+controlKeyboard()
+
+
+
